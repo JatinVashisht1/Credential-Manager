55d959be7e192606d8eacffc6e28f21b1fa6a544
JatinVashisht1 <jatin8085vashisht@gmail.com>
2022-05-09 23:58:20 +0530

    updated encryption login

--- app/src/main/java/com/jatinvashisht/credentialmanager/data/repository/CredentialRepositoryImpl.kt
+++ app/src/main/java/com/jatinvashisht/credentialmanager/data/repository/CredentialRepositoryImpl.kt
@@ -1,5 +1,7 @@
 package com.jatinvashisht.credentialmanager.data.repository
 
+import android.security.keystore.KeyGenParameterSpec
+import android.security.keystore.KeyProperties
 import android.util.Log
 import com.jatinvashisht.credentialmanager.core.Resource
 import com.jatinvashisht.credentialmanager.data.local.CredentialDatabase
@@ -10,6 +12,7 @@ import kotlinx.coroutines.flow.Flow
 import kotlinx.coroutines.flow.collectLatest
 import kotlinx.coroutines.flow.flow
 import java.lang.Exception
+import java.security.*
 import javax.crypto.Cipher
 import javax.crypto.KeyGenerator
 import javax.inject.Inject
@@ -18,12 +21,14 @@ import javax.inject.Inject
 class CredentialRepositoryImpl @Inject constructor(
     private val credentialDatabase: CredentialDatabase
 ) : CredentialRepository {
-    val job: Job? = null
     override suspend fun insertCredential(credentialEntity: CredentialEntity) {
-
         val credentialEntityToSave = encryptCredential(credentialEntity = credentialEntity)
         Log.d("repository", "credentialEntityToSave is $credentialEntityToSave")
-        credentialDatabase.dao.insertCredential(credentialEntity = credentialEntityToSave)
+        val digestedCredentialEntity = createMessageDigest(credentialEntity = credentialEntityToSave)
+//        val signedCredentials = generateDigitalSignature(credentialEntity = credentialEntityToSave)
+//        Log.d("repository", "signed credentials is $signedCredentials")
+
+        credentialDatabase.dao.insertCredential(credentialEntity = digestedCredentialEntity)
     }
 
     override suspend fun deleteCredential(credentialEntity: CredentialEntity) {
@@ -56,6 +61,7 @@ class CredentialRepositoryImpl @Inject constructor(
         }
     }
 
+    // utility function to encrypt credentials
     private fun encryptCredential(credentialEntity: CredentialEntity): CredentialEntity{
         val keygen = KeyGenerator.getInstance("AES")
         keygen.init(256)
@@ -63,13 +69,51 @@ class CredentialRepositoryImpl @Inject constructor(
         val cipher = Cipher.getInstance("AES/CBC/PKCS5PADDING")
         cipher.init(Cipher.ENCRYPT_MODE, key)
         val cipherText = cipher.doFinal(credentialEntity.credentialInfo.toByteArray())
+        val credentialTitle = cipher.doFinal(credentialEntity.credentialTitle.toByteArray())
         val iv = cipher.iv
-        Log.d("repository", "value of cipherText is $cipherText and value of iv if $iv")
+        Log.d("repository", "value of cipherText is ${String(cipherText)} and value of iv if ${String(iv)}")
         val credentialEntityToSave = CredentialEntity(
-            credentialTitle = credentialEntity.credentialTitle,
-            credentialInfo = String(iv)
+            credentialTitle = String(cipherText),
+            credentialInfo = String(credentialTitle)
         )
-        Log.d("mytag", "credentialEntityToSave is $credentialEntityToSave")
+//        Log.d("mytag", "credentialEntityToSave is $credentialEntityToSave")
         return credentialEntityToSave
     }
+    private fun createMessageDigest(credentialEntity: CredentialEntity): CredentialEntity {
+        val credentialTitleMessage = credentialEntity.credentialTitle
+        val credentialInfoMessage = credentialEntity.credentialInfo
+
+        val messageDigest = MessageDigest.getInstance("SHA-256")
+        val credentialTitleDigest = messageDigest.digest(credentialTitleMessage.toByteArray())
+        val credentialInfoDigest = messageDigest.digest(credentialInfoMessage.toByteArray())
+
+        val digestedCredentialEntity = CredentialEntity(
+            credentialTitle =  String(credentialTitleDigest),
+            credentialInfo = String(credentialInfoDigest)
+        )
+
+        return digestedCredentialEntity
+    }
+
+/*
+private fun generateDigitalSignature(credentialEntity: CredentialEntity): ByteArray? {
+val credentialInfoMessage = credentialEntity.credentialInfo
+val credentialTitleMessage = credentialEntity.credentialTitle
+
+//        val keyPairGenerator = KeyPairGenerator.getInstance(KeyProperties.KEY_ALGORITHM_AES, "AndroidKeyStore")
+//        val parameterSpec = KeyGenParameterSpec.Builder(
+//            KeyProperties.PURPOSE_SIGN or KeyProperties.PURPOSE_VERIFY
+//        )
+
+
+val signature = Signature.getInstance("SHA256withECDSA")
+.apply {
+initSign(key)
+update(credentialInfoMessage.toByteArray())
+update(credentialTitleMessage.toByteArray())
+}
+val signedSignature = signature.sign()
+return signedSignature
+}
+*/
 }
